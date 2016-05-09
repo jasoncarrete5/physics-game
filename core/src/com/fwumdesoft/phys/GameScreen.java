4a414ad873f4717f1fe55ee793b94c767a843d86
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-09 19:51:19 -0400

    Added driver class and game screen

---
+++ core/src/com/fwumdesoft/phys/GameScreen.java
@@ -0,0 +1,7 @@
+package com.fwumdesoft.phys;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+	
+}
--- core/src/com/fwumdesoft/phys/Main.java
+++ core/src/com/fwumdesoft/phys/Main.java
@@ -1,27 +1,28 @@
 package com.fwumdesoft.phys;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
-public class Main extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Main extends Game {
+	public static AssetManager assets;
+	public static Skin uiskin;
+	public static Game game;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		game = this;
+		uiskin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+		assets = new AssetManager();
+		
+		setScreen(new GameScreen());
 	}
-
+	
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void dispose() {
+		super.dispose();
+		uiskin.dispose();
+		assets.dispose();
 	}
 }
