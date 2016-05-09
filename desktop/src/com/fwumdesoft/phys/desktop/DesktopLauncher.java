10379f83bea1e214077f9fcf3c75ce273ab9adb1
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-09 19:39:29 -0400

    Gave the game a title

--- desktop/src/com/fwumdesoft/phys/desktop/DesktopLauncher.java
+++ desktop/src/com/fwumdesoft/phys/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.fwumdesoft.phys.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Wavefront";
 		new LwjglApplication(new Main(), config);
 	}
 }
