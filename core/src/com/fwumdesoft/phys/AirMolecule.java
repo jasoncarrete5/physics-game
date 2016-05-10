02ff85275b763773545927bf9d477c52bc844c62
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-10 00:04:52 -0400

    Added hitbox to AirMolecule class

--- core/src/com/fwumdesoft/phys/AirMolecule.java
+++ core/src/com/fwumdesoft/phys/AirMolecule.java
@@ -3,7 +3,9 @@ package com.fwumdesoft.phys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
 public class AirMolecule extends Actor implements Poolable {
@@ -13,11 +15,15 @@ public class AirMolecule extends Actor implements Poolable {
 		texture = new TextureRegion(Main.assets.get("textures/air.png", Texture.class));
 	}
 	
+	private Polygon hitbox;
+	
 	/**
 	 * No-arg constructor required for object pooling.
 	 */
 	public AirMolecule() {
 		setSize(1.5f, 1.5f);
+		setOrigin(Align.center);
+		hitbox = new Polygon(new float[] {0, 0, getWidth(), 0, getWidth(), getHeight(), 0, getHeight()});
 	}
 	
 	@Override
@@ -26,7 +32,17 @@ public class AirMolecule extends Actor implements Poolable {
 	}
 	
 	@Override
+	protected void positionChanged() {
+		hitbox.setPosition(getX(), getY());
+	}
+	
+	@Override
+	protected void rotationChanged() {
+		hitbox.setRotation(getRotation());
+	}
+	
+	@Override
 	public void reset() {
-		//Nothing to reset
+		hitbox.dirty();
 	}
 }
