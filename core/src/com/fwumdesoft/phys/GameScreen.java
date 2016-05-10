76b702967ae146c3da2a0203329f326cdec15fb9
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-09 20:55:29 -0400

    Added GameScreen class

--- core/src/com/fwumdesoft/phys/GameScreen.java
+++ core/src/com/fwumdesoft/phys/GameScreen.java
@@ -1,7 +1,41 @@
 package com.fwumdesoft.phys;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 
 public class GameScreen extends ScreenAdapter {
+	private Stage stage;
 	
+	@Override
+	public void show() {
+		stage = new Stage(new FillViewport(200f, 200f * ((float)Gdx.graphics.getHeight() / Gdx.graphics.getWidth())));
+		Gdx.input.setInputProcessor(stage);
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.75f, 0.75f, 0.75f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);
+		stage.draw();
+	}
+	
+	@Override
+	public void hide() {
+		dispose();
+	}
+	
+	@Override
+	public void dispose() {
+		stage.dispose();
+	}
 }
