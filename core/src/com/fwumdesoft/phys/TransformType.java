f0583aa28d1533081bfea89f73ffeac71a0b487e
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-10 23:16:42 -0400

    Tweaked bit masks

--- core/src/com/fwumdesoft/phys/TransformType.java
+++ core/src/com/fwumdesoft/phys/TransformType.java
@@ -4,6 +4,8 @@ package com.fwumdesoft.phys;
  * Encapsulates bit masks for determining if a reflector/refractor is fixed.
  */
 public class TransformType {
-	public static final byte positionFixed = 0b01;
-	public static final byte rotationFixed = 0b10;
+	public static final byte positionFixed = 1 << 0;
+	public static final byte rotationFixed = 1 << 1;
+	
+	public static final byte fixed = positionFixed | rotationFixed;
 }
