40eef409ff73d73279a9e2824513c77e9dae849c
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-10 21:42:23 -0400

    Added TransformType class

---
+++ core/src/com/fwumdesoft/phys/TransformType.java
@@ -0,0 +1,9 @@
+package com.fwumdesoft.phys;
+
+/**
+ * Encapsulates bit masks for determining if a reflector/refractor is fixed.
+ */
+public class TransformType {
+	public static final byte positionFixed = 0b01;
+	public static final byte rotationFixed = 0b10;
+}
