5eeb74d9f15ed0e2421d0ba96041d916bc69fe1a
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-10 23:17:28 -0400

    Implemented a Reflector

--- core/src/com/fwumdesoft/phys/actors/Reflector.java
+++ core/src/com/fwumdesoft/phys/actors/Reflector.java
@@ -1,15 +1,36 @@
 package com.fwumdesoft.phys.actors;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Align;
 import com.fwumdesoft.phys.Main;
 
-public class Reflector extends Actor {
+public class Reflector extends HitboxActor {
 	private static final TextureRegion texture;
 	
 	static {
 		texture = new TextureRegion(Main.asset.get(Main.Assets.REFLECTOR));
 	}
 	
+	private byte fixed;
 	
+	/**
+	 * Instantiates a new Reflector with the specified fixation.
+	 * @param fixedType A bit mask value setting the fixation of the Reflector. Used
+	 * to determine what kind of modifications the user can make to this Reflector.
+	 */
+	public Reflector(byte fixedType) {
+		setSize(5, 20);
+		setOrigin(Align.center);
+		fixed = fixedType;
+	}
+	
+	@Override
+	public void draw(Batch batch, float parentAlpha) {
+		batch.draw(texture, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+	}
+	
+	public byte getFixedMask() {
+		return fixed;
+	}
 }
