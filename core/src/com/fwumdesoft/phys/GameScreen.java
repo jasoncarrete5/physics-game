98d2002907e9325edf816e9767263ffe62857111
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-10 23:18:18 -0400

    Implemented some AirMolecule interactions with reflectors

--- core/src/com/fwumdesoft/phys/GameScreen.java
+++ core/src/com/fwumdesoft/phys/GameScreen.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Pools;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.fwumdesoft.phys.actors.AirMolecule;
+import com.fwumdesoft.phys.actors.Reflector;
 import com.fwumdesoft.phys.actors.Wave;
 
 public class GameScreen extends ScreenAdapter {
@@ -24,6 +25,10 @@ public class GameScreen extends ScreenAdapter {
 		Wave wave = new Wave();
 		stage.addActor(wave);
 		wave.propagate(0, 0, 20);
+		
+		Reflector refl = new Reflector(TransformType.fixed);
+		refl.setPosition(100, 30, Align.center);
+		stage.addActor(refl);
 	}
 	
 	/**
@@ -41,8 +46,9 @@ public class GameScreen extends ScreenAdapter {
 			for(int minX = 0; minX < worldWidth; minX += cellLength) {
 				float randX = MathUtils.random(minX, minX + cellLength);
 				float randY = MathUtils.random(minY, minY + cellLength);
-				Actor airMolecule = Pools.get(AirMolecule.class, totalAir).obtain();
+				AirMolecule airMolecule = Pools.get(AirMolecule.class, totalAir).obtain();
 				airMolecule.setPosition(randX, randY, Align.center);
+				airMolecule.markPosition(new Vector2(airMolecule.getX(), airMolecule.getY())); //marks original position
 				stage.addActor(airMolecule);
 			}
 		}
--- core/src/com/fwumdesoft/phys/actors/AirMolecule.java
+++ core/src/com/fwumdesoft/phys/actors/AirMolecule.java
@@ -2,6 +2,11 @@ package com.fwumdesoft.phys.actors;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.fwumdesoft.phys.Main;
@@ -12,6 +17,8 @@ public class AirMolecule extends HitboxActor implements Poolable {
 	static {
 		texture = new TextureRegion(Main.asset.get(Main.Assets.AIR));
 	}
+	
+	private Vector2 markedPos;
 	
 	/**
 	 * No-arg constructor required for object pooling.
@@ -24,6 +31,33 @@ public class AirMolecule extends HitboxActor implements Poolable {
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
 		batch.draw(texture, getX(), getY(), getOriginX(), getOriginY(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+	}
+	
+	@Override
+	public void positionChanged() {
+		super.positionChanged();
+		
+		//Check if molecule has collided with a reflector
+		if(getStage() == null) return;
+		for(Actor actor : getStage().getActors()) {
+			if(actor instanceof Reflector) {
+				Reflector refl = (Reflector)actor;
+				//air molecule should reset position when they hit a reflector so the Wave can cause them to displace
+				if(Intersector.overlapConvexPolygons(hitbox(), refl.hitbox())) { 
+					clearActions();
+					MoveToAction moveAction = Actions.moveTo(markedPos.x, markedPos.y);
+					addAction(moveAction);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Marks an important position for this AirMolecule. (ex. original position).
+	 * @param pos Position to be marked.
+	 */
+	public void markPosition(Vector2 pos) {
+		markedPos = pos;
 	}
 	
 	@Override
