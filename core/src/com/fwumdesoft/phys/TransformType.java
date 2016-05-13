4068e33a770d18fb8b69a3f2c45887083cf82ebe
Jason C <jasoncarrete5@gmail.com>
2016-05-13 06:59:13 -0400

    Made TransformType an interface for convenience

--- core/src/com/fwumdesoft/phys/TransformType.java
+++ core/src/com/fwumdesoft/phys/TransformType.java
@@ -3,9 +3,12 @@ package com.fwumdesoft.phys;
 /**
  * Encapsulates bit masks for determining if a reflector/refractor is fixed.
  */
-public class TransformType {
+public interface TransformType {
 	public static final byte positionFixed = 1 << 0;
 	public static final byte rotationFixed = 1 << 1;
 	
 	public static final byte fixed = positionFixed | rotationFixed;
+	
+	byte getFixed();
+	void setFixed(byte fixed);
 }
