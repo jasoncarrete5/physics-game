2eafd88a0aac041db68dcfe66f226cadaf0a72a1
Jason Carrete <jasoncarrete5@gmail.com>
2016-05-14 16:00:11 -0400

    Can now save and load levels

--- core/src/com/fwumdesoft/phys/EditorScreen.java
+++ core/src/com/fwumdesoft/phys/EditorScreen.java
@@ -20,8 +20,8 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.DragListener;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.fwumdesoft.phys.actors.HitboxActor;
 import com.fwumdesoft.phys.actors.Reflector;
 
 /**
@@ -36,44 +36,9 @@ public class EditorScreen extends ScreenAdapter {
 		stage = new Stage(new FillViewport(1000f, 1000f * ((float)Gdx.graphics.getHeight() / Gdx.graphics.getWidth())));
 		Gdx.input.setInputProcessor(stage);
 		
-		//ask the user to load a level or create a new level
-		Dialog newLevel = new Dialog("Level Editor", Main.uiskin) {
-			@Override
-			protected void result(Object object) {
-//				TextField txtFileName = (TextField)object;
-//				FileHandle lvlFile = Gdx.files.external("physics-game/levels/" + txtFileName.getText());
-//				if(lvlFile.exists()) {
-//					level = Level.loadFromFile(txtFileName.getText());
-//				} else {
-//					level = new Level();
-//					level.setName(txtFileName.getText());
-//				}
-			}
-		};
-		TextField txtFileName = new TextField("", Main.uiskin);
-		txtFileName.setMessageText("filename");
-		newLevel.button("Open Level", new Runnable() {
-			public void run() {
-				FileHandle lvlFile = Gdx.files.local("levels/" + txtFileName.getText());
-				if(!lvlFile.exists()) {
-					new Dialog("Error", Main.uiskin).text("Failed to load file").button("Ok").show(stage);
-				} else {
-					level = Level.loadFromFile(lvlFile.name());
-				}
-			}
-		});
-		newLevel.button("New Level", new Runnable() {
-			public void run() {
-				FileHandle lvlFile = Gdx.files.local("levels/" + txtFileName.getText());
-				lvlFile.delete();
-				level = new Level();
-				level.setName(lvlFile.name());
-			}
-		});
-		newLevel.add(txtFileName);
-		newLevel.show(stage);
 		
-		//set upactor settings window
+		
+		//set up actor settings window
 		Window wndActorSettings = new Window("Actor Settings", Main.uiskin);
 		CheckBox chkFixedPosition = new CheckBox("Fixed Position", Main.uiskin);
 		chkFixedPosition.addListener(new ChangeListener() {
@@ -81,8 +46,8 @@ public class EditorScreen extends ScreenAdapter {
 			public void changed(ChangeEvent event, Actor actor) {
 				CheckBox chkBox = (CheckBox)event.getListenerActor();
 				Actor a = (Actor)event.getListenerActor().getParent().getUserObject();
-				if(a instanceof TransformType) {
-					TransformType tActor = (TransformType)a;
+				if(a instanceof HitboxActor) {
+					HitboxActor tActor = (HitboxActor)a;
 					if(chkBox.isChecked()) {
 						tActor.setFixed((byte)(tActor.getFixed() | TransformType.positionFixed));
 					} else {
@@ -97,8 +62,8 @@ public class EditorScreen extends ScreenAdapter {
 			public void changed(ChangeEvent event, Actor actor) {
 				CheckBox chkBox = (CheckBox)event.getListenerActor();
 				Actor a = (Actor)event.getListenerActor().getParent().getUserObject();
-				if(a instanceof TransformType) {
-					TransformType tActor = (TransformType)a;
+				if(a instanceof HitboxActor) {
+					HitboxActor tActor = (HitboxActor)a;
 					if(chkBox.isChecked()) {
 						tActor.setFixed((byte)(tActor.getFixed() | TransformType.rotationFixed));
 					} else {
@@ -114,10 +79,16 @@ public class EditorScreen extends ScreenAdapter {
 		wndActorSettings.pack();
 		wndActorSettings.setPosition(0, stage.getHeight(), Align.topLeft);
 		
+		
+		//set up actor window
 		DragListener moveWhenDragged = new DragListener() {
 			@Override
 			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-				wndActorSettings.setUserObject(event.getListenerActor());
+				//update wndActorSettings
+				wndActorSettings.setUserObject(event.getListenerActor()); //set the object to currently selected Actor
+				HitboxActor tActor = (HitboxActor)event.getListenerActor();
+				chkFixedPosition.setChecked((tActor.getFixed() & TransformType.positionFixed) != 0);
+				chkFixedRotation.setChecked((tActor.getFixed() & TransformType.rotationFixed) != 0);
 				return super.touchDown(event, x, y, pointer, button);
 			}
 			
@@ -127,8 +98,6 @@ public class EditorScreen extends ScreenAdapter {
 				event.getListenerActor().addAction(moveAction);
 			}
 		};
-		
-		//set up actor window
 		Window wndActors = new Window("Actors", Main.uiskin);
 		TextButton btnReflector = new TextButton("Reflector", Main.uiskin);
 		btnReflector.addListener(new ClickListener(Buttons.LEFT) {
@@ -138,7 +107,7 @@ public class EditorScreen extends ScreenAdapter {
 				refl.addListener(moveWhenDragged);
 				refl.setPosition(stage.getWidth() / 2, stage.getHeight() / 2, Align.center);
 				stage.addActor(refl);
-//				level.add(refl);
+				level.add(refl);
 			}
 		});
 		wndActors.add(btnReflector);
@@ -146,12 +115,46 @@ public class EditorScreen extends ScreenAdapter {
 		
 		stage.addActor(wndActors);
 		stage.addActor(wndActorSettings);
-	}
-	
-	private void save() {
-		FileHandle lvlFile = Gdx.files.external("physics-game/levels/" + level.getName());
-		lvlFile.delete();
-		level.writeLevel();
+		
+		
+		//ask the user to load a level or create a new level
+		Dialog newLevel = new Dialog("Level Editor", Main.uiskin) {
+			@Override
+			protected void result(Object object) {
+				Runnable r = (Runnable)object;
+				r.run();
+			}
+		};
+		TextField txtFileName = new TextField("", Main.uiskin);
+		txtFileName.setMessageText("filename");
+		newLevel.button("Open Level", new Runnable() {
+			public void run() {
+				FileHandle lvlFile = Gdx.files.local("levels/" + txtFileName.getText());
+				if(!lvlFile.exists()) {
+					new Dialog("Error", Main.uiskin) {
+						protected void result(Object object) {
+							Main.game.setScreen(new MainMenuScreen());
+						}
+					}.text("Failed to load file").button("Ok").show(stage);
+				} else {
+					level = Level.loadFromFile(lvlFile.name());
+					level.setupStage(stage, true);
+					for(Actor a : stage.getActors()) {
+						a.addListener(moveWhenDragged);
+					}
+				}
+			}
+		});
+		newLevel.button("New Level", new Runnable() {
+			public void run() {
+				FileHandle lvlFile = Gdx.files.local("levels/" + txtFileName.getText());
+				lvlFile.delete();
+				level = new Level();
+				level.setName(lvlFile.name());
+			}
+		});
+		newLevel.add(txtFileName);
+		newLevel.show(stage);
 	}
 	
 	@Override
@@ -170,12 +173,12 @@ public class EditorScreen extends ScreenAdapter {
 	
 	@Override
 	public void hide() {
-		save();
+		if(level != null) level.writeLevel();
 		dispose();
 	}
 	
 	@Override
 	public void dispose() {
-		stage.dispose();
+		if(stage != null) stage.dispose();
 	}
 }
